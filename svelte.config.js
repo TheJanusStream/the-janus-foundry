8c137fcf47b0e639a0c5551ed01ab75e884b8ccc
TheJanusStream <thejanusstream@gmail.com>
2025-11-02 13:15:07 +0100

    Fix gh-pages paths and bump Version to 1.0.5

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,23 @@
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: "index.html",
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
     }),
+    paths: {
+      // Use a specific environment variable for the GitHub Pages base path
+      base: process.env.IS_GH_PAGES ? '/the-janus-foundry' : '',
+    }
   },
 };
 
